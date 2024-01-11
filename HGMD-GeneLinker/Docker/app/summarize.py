42fc812b86196486f7fc7bd44267e4ed870b86bc
ysut <yshutn@gmail.com>
2024-01-11 21:34:03 +0900

    update

--- HGMD-GeneLinker/Docker/app/summarize.py
+++ HGMD-GeneLinker/Docker/app/summarize.py
@@ -4,50 +4,51 @@ import pandas as pd
 import pymysql
 
 
-## Set HGMD Version for output
-hgmd_ver: str = sys.argv[1]
-output_path = f"{sys.argv[2]}/HGMD_GeneBasedInfo_{hgmd_ver}.tsv.gz"
+def main():
+    ## Set HGMD Version for output
+    hgmd_ver: str = sys.argv[1]
+    output_path = f"{sys.argv[2]}/HGMD_GeneBasedInfo_{hgmd_ver}.tsv.gz"
 
+    ## Connection
+    database = 'hgmd_pro'
+    mariadb_params = {
+        'host': 'db', 'user': 'root', 'password': 'pwd',
+        'port': 3306, 'database': database, 
+        'cursorclass': pymysql.cursors.DictCursor
+        }
+    connection = pymysql.connect(**mariadb_params)
 
-## Connection
-database = 'hgmd_pro'
-mariadb_params = {
-    'host': 'db', 'user': 'root', 'password': 'pwd',
-    'port': 3306, 'database': database, 
-    'cursorclass': pymysql.cursors.DictCursor
-    }
-connection = pymysql.connect(**mariadb_params)
+    ## Create tags DataFrame
+    sql = f"SELECT gene, tag FROM allmut;"
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        fetched_allmut = cursor.fetchall()
 
+    df_allmut = pd.DataFrame(fetched_allmut)
 
-## Create tags DataFrame
-sql = f"SELECT gene, tag FROM allmut;"
-with connection.cursor() as cursor:
-    cursor.execute(sql)
-    fetched_allmut = cursor.fetchall()
+    df_tags = df_allmut.pivot_table(
+        index='gene', columns='tag', aggfunc='size', fill_value=0
+        )
+    df_tags = df_tags.reset_index()
+    df_tags.columns.name = None
 
-df_allmut = pd.DataFrame(fetched_allmut)
+    ## Create all genes DataFrame
+    sql = f"SELECT gene, altsymbol, refseq, expected_inheritance, hgncID, omimid FROM allgenes;"
 
-df_tags = df_allmut.pivot_table(
-    index='gene', columns='tag', aggfunc='size', fill_value=0
-    )
-df_tags = df_tags.reset_index()
-df_tags.columns.name = None
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        fetched_allgenes = cursor.fetchall()
 
+    df_genes = pd.DataFrame(fetched_allgenes)
 
-## Create all genes DataFrame
-sql = f"SELECT gene, altsymbol, refseq, expected_inheritance, hgncID, omimid FROM allgenes;"
+    ## Left join
+    df = pd.merge(left=df_genes, right=df_tags, how='left', on='gene')
 
-with connection.cursor() as cursor:
-    cursor.execute(sql)
-    fetched_allgenes = cursor.fetchall()
+    ## Save a file as compressed TSV
+    df.to_csv(
+        output_path, index=False, sep='\t', encoding='utf-8', compression='gzip'
+        )
+    
 
-df_genes = pd.DataFrame(fetched_allgenes)
-
-## Left join
-df = pd.merge(left=df_genes, right=df_tags, how='left', on='gene')
-
-
-## Save a file as compressed TSV
-df.to_csv(
-    output_path, index=False, sep='\t', encoding='utf-8', compression='gzip'
-    )
+if __name__ == '__main__':
+    main()
