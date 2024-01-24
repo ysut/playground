d90c87282f0ca8628ba1d21d4bee358d97c0abb1
ysut <yshutn@gmail.com>
2024-01-24 19:30:15 +0900

    update

---
+++ dev/ch1-3/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    for i in 1..51 {
+        if i % 3 == 0 {
+            println!("A");
+            continue;
+        }
+        if i >= 30 && i <= 39 {
+            println!("A");
+            continue;
+        }
+        if i % 10 == 3 {
+            println!("A");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
