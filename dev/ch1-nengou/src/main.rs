64fc4e20ff3b69d9c7adce5220d364e7b223fe6c
ysut <yshutn@gmail.com>
2024-01-24 19:48:22 +0900

    update

--- dev/ch1-nengou/src/main.rs
+++ dev/ch1-nengou/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    for i in 1926..2027 {
+        println!("{}年", i);
+        if i == 1930 {
+            println!("昭和元年");
+        } else if i == 2019 {
+            println!("令和元年");
+        }
+    }
 }
