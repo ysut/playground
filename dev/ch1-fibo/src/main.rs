edcc55a8ffa725779fbe5535108b53364f8e07cf
ysut <yshutn@gmail.com>
2024-01-28 20:13:37 +0900

    update

--- dev/ch1-fibo/src/main.rs
+++ dev/ch1-fibo/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut a = 0;
+    let mut b = 1;
+    let mut c = 0;
+    for _ in 0..10 {
+        c = a + b;
+        a = b;
+        b = c;
+        println!("{}", c);
+    }
 }
