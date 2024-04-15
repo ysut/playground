0ebb3ff6c33aeada2e2097613cd08c09ec2aad7b
ysut <yshutn@gmail.com>
2024-04-15 20:01:49 +0900

    update

---
+++ wesanno/wesanno/libs/annolib/addtionalanno.py
@@ -0,0 +1,7 @@
+from wesanno.libs.annolib.gnomad import Gnomad
+from wesanno.libs.annolib.decipher import Decipher
+from wesanno.libs.annolib.splai import SplAI
+from wesanno.libs.annolib.jrvs import Jrvs
+
+def gnomad_annotation(*args):
+    dataframe = 
--- wesanno/wesanno/libs/annolibs/addtionalanno.py
+++
@@ -1,7 +0,0 @@
-from wesanno.libs.annolibs.gnomad import Gnomad
-from wesanno.libs.annolibs.decipher import Decipher
-from wesanno.libs.annolibs.splai import SplAI
-from wesanno.libs.annolibs.jrvs import Jrvs
-
-def gnomad_annotation(*args):
-    dataframe = 
