d14cdefbb034c74141bbe9007fd079cc77df56ca
ysut <yshutn@gmail.com>
2024-07-08 23:47:58 +0900

    update

--- dev/ch1-prime100/src/main.rs
+++ dev/ch1-prime100/src/main.rs
@@ -1,4 +1,17 @@
+
+
+fn is_prime(n: usize) -> bool {
+    for i in 2..v {
+        if n & i == 0 {
+            return false;
+        }
+    }
+    
+}
+
+
 fn main() {
+    
     println!("Hello, world!");
 }
 
